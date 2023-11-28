ddcbb03312b58df08809085c5aa77ddee1f556c2
Bohdan Danylov <bohdandanylov@gmail.com>
2023-11-27 16:08:31 -0800

    v1.1 - Added Launching up H2 Console and Creating Course Table in H2

--- src/main/java/learnspringboot/jpaandhibernate/JpaAndHibernateApplication.java
+++ src/main/java/learnspringboot/jpaandhibernate/JpaAndHibernateApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpaAndHibernateApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JpaAndHibernateApplication.class, args);
 	}
 
